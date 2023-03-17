1203cb46965a428b6a3b80a5aa8bdb6fdf0c204b
Creativity12345 <sebastianm444@gmail.com>
2023-03-17 14:00:25 +0100

    change environment key

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,10 +1,9 @@
 export const environment = {
   firebase: {
-    projectId: 'slack-clone-5e282',
-    appId: '1:752266991143:web:11184e1d1b08fb36bce002',
-    databaseURL: 'https://slack-clone-5e282-default-rtdb.europe-west1.firebasedatabase.app',
-    storageBucket: 'slack-clone-5e282.appspot.com',
-    apiKey: 'AIzaSyC-iuVA542l9ICAfAmJ5uDcAPrq46m8XrE',
-    authDomain: 'slack-clone-5e282.firebaseapp.com',
-    messagingSenderId: '752266991143',
+    apiKey: "AIzaSyAtRQdVdqfKnXf8HB10GrO8w888uTDQrFE",
+    authDomain: "slack-clone-e5430.firebaseapp.com",
+    projectId: "slack-clone-e5430",
+    storageBucket: "slack-clone-e5430.appspot.com",
+    messagingSenderId: "315212743431",
+    appId: "1:315212743431:web:4467646ec3a59fe7417ba2"
   },};
