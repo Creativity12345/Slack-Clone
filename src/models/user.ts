87d44e41c1274ecac106ea1e6a020b1e5a293554
Creativity12345 <sebastianm444@gmail.com>
2023-03-17 16:54:20 +0100

    change ids

--- src/models/user.ts
+++ src/models/user.ts
@@ -8,7 +8,7 @@ export let user = {
         "profileImg": '../../assets/img/blank-profile.png'
     },
     "communicationSections": {
-        "channels": ['hTdApKhXg8111SoNr2Yq', 'KlTnEdj7XuVLzYnB13Iw'],
+        "channels": ['TLmES9PTC8aEAw9aGIa4', 'n5Pg6Aiok7cmnQ1IBFG9'],
         "groups": [],
         "directMessages": []
     },
